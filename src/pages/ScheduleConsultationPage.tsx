b82f77f418c5ba26fd0b8e1c531ddcbadfee5ff1
tahatms12 <tahatms@gmail.com>
2025-07-12 18:54:56 +0500

    feat: Update Content Security Policy in _headers and embed Zoho calendar

--- src/pages/ScheduleConsultationPage.tsx
+++ src/pages/ScheduleConsultationPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ScheduleConsultationPage: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Schedule Consultation Page</h1>
+      <iframe src="https://calendar.zoho.com/eventreqForm/zz08011230b90f11f7d9e4f845d3466bf542b203c85792616eef005cce40c051b56dd6ff3b7f21ca86a6ce7ce3b245f62337f9fb78?theme=0&l=en&tz=America%2FToronto" title="taha"frameBorder="0" scrolling="no" marginWidth="0" border="0px" marginHeight="0"height="350px" width="432px" allowTransparency="true" align="center"></iframe>
     </div>
   );
 };
