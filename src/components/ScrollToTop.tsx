b203b06d18ea719fbc0e1cf011f14c7065e5aa53
Muhammad Ammar <ammarwhy98@gmail.com>
2025-11-28 00:30:29 +0500

    Create ScrollToTop.tsx
    Scroll to top functionality

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,14 @@
+// src/components/ScrollToTop.tsx
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top instantly on route change
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
