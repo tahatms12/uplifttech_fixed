2f182b8759b6ef1d39bcc733f8f43e0ade42edb4
tahatms12 <tahatms@gmail.com>
2025-11-28 06:46:21 +0500

    Update logo image source URL

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <img 
-        src="https://24vzlu2kzs.ufs.sh/f/4JlBnp1v6U48S8sVkrTs0v8bxAn6gqLm4y5oP7kC9HFw2pRh" 
+        src="https://24vzlu2kzs.ufs.sh/f/4JlBnp1v6U48Q8fcNsj55N2MEH8Cfy0GjWI4bpTvXcdYzLkF" 
         alt="Uplift Technologies Logo" 
         width={667}
         height={196}
